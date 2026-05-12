0b9b6d504a5fb3b071427e61ce96f5e690221781
MchalW2406 <michalwoj07@gmail.com>
2026-05-12 11:31:53 +0300

    plane.gd

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var cooldown = 0
 var started = false
+var ended = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,21 +13,35 @@ func _process(delta: float) -> void:
 	if started:
 		if cooldown > 0:
 			cooldown -= 1
-			
-		if position.y >= DisplayServer.window_get_size().y / 2 or position.y <= (0 - DisplayServer.window_get_size().y) / 2:
+		
+		if position.y >= 640 or position.y <= 0: 
 			velocity.y = 0
 			#get_tree().change_scene_to_file("res://scenes/flyingGameOver.tscn")
 			var gameOver = load("res://scenes/flyingGameOver.tscn")
 			var gameOverScreen = gameOver.instantiate()
 			get_tree().current_scene.add_child(gameOverScreen)
-			gameOverScreen.position = Vector2(0, 0)
+			gameOverScreen.position = Vector2(586, 333)
 			get_tree().paused = true
 			#get_tree().reload_current_scene()
+		
+	if get_tree().current_scene.get_node("ProgressBar").value >= 100 and !ended:
+		ended = true
+		get_tree().current_scene.get_node("Start").text = "Congratulations!"
+		get_tree().current_scene.get_node("Start").visible = true
+		get_tree().current_scene.get_node("Start").set("theme_override_colors/font_color", Color(0.461, 1.0, 0.429, 1.0))
+		
+		
+	if ended:
+		position.x += 7
+		if position.x >= 1500:
+			get_tree().change_scene_to_file("res://scenes/finnish_map.tscn")
+	
 	
 func _physics_process(delta: float) -> void:
-	if started:
+	if started and !ended:
 		var gravity = get_gravity()
-		velocity.y += gravity.y * delta
+		gravity = 1480.0
+		velocity.y += gravity * delta
 		
 		move_and_slide()
 		for i in get_slide_collision_count():
@@ -35,12 +50,12 @@ func _physics_process(delta: float) -> void:
 			var gameOver = load("res://scenes/flyingGameOver.tscn")
 			var gameOverScreen = gameOver.instantiate()
 			get_tree().current_scene.add_child(gameOverScreen)
-			gameOverScreen.position = Vector2(0, 0)
+			gameOverScreen.position = Vector2(586, 303)
 			get_tree().paused = true
-
+	
 func _input(ev):
 	
-	if Input.is_key_pressed(KEY_SPACE) and cooldown == 0:
+	if Input.is_key_pressed(KEY_SPACE) and cooldown == 0 and !ended:
 		if !started:
 			started = true
 		print("Space")
