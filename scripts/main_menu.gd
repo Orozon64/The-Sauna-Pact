85c5b1911619c358b279a1d15ea938a407d5466a
Orozon64 <ignacy.guminiak@tm1.edu.pl>
2026-05-20 08:56:59 +0300

    made menu working

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,9 +3,9 @@ extends VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$PlayButton.button_down.connect(_start_game)
-	$CreditsButton.button_down.connect(_show_credits)
-	$ReturnButton.button_down.connect(_return_to_menu)
+	$CenterContainer/VBoxContainer/PlayButton.button_down.connect(_start_game)
+	$CenterContainer/VBoxContainer/CreditsButton.button_down.connect(_show_credits)
+	$CenterContainer/VBoxContainer/ReturnButton.button_down.connect(_return_to_menu)
 
 func _start_game():
 	var initial_data = {
@@ -29,13 +29,13 @@ func _start_game():
 	get_tree().change_scene_to_file("res://scenes/intro_scene.tscn")
 
 func _show_credits():
-	$CreditsButton.hide()
-	$PlayButton.hide()
-	$ReturnButton.show()
-	$Label.text = "Programming: Ignacy Guminiak, Michał Wójtowicz, Zuzanna Dróżdż, Jan Kukier\nNetwork management: Jan Kukier, Jan Maciejewski, Jakub Klimowicz\nGraphic design: Michał Wójtowicz, Zuzanna Dróżdż, Jan Maciejewski\nAudio design: Jakub Klimowicz, Jan Sarnecki, Ignacy Guminiak\nProject leader: Jakub Klimowicz\nSpecial thanks to mechatronics team :)\n"
-	$Label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	$CenterContainer/VBoxContainer/PlayButton.hide()
+	$CenterContainer/VBoxContainer/CreditsButton.hide()
+	$CenterContainer/VBoxContainer/ReturnButton.show()
+	$CenterContainer/VBoxContainer/Label.text = "Programming: Ignacy Guminiak, Michał Wójtowicz, Zuzanna Dróżdż, Jan Kukier\nNetwork management: Jan Kukier, Jan Maciejewski, Jakub Klimowicz\nGraphic design: Michał Wójtowicz, Zuzanna Dróżdż, Jan Maciejewski\nAudio design: Jakub Klimowicz, Jan Sarnecki, Ignacy Guminiak\nProject leader: Jakub Klimowicz\nSpecial thanks to mechatronics team :)\n"
+	$CenterContainer/VBoxContainer/Label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 func _return_to_menu():
-	$CreditsButton.show()
-	$PlayButton.show()
-	$ReturnButton.hide()
-	$Label.text = "The Sauna Pact"
+	$CenterContainer/VBoxContainer/PlayButton.show()
+	$CenterContainer/VBoxContainer/CreditsButton.show()
+	$CenterContainer/VBoxContainer/ReturnButton.hide()
+	$CenterContainer/VBoxContainer/Label.text = "The Sauna Pact"
